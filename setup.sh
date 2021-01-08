d2e90532c61070ad970228bef248977ce1f34396
swoitge <66548513+swoitge@users.noreply.github.com>
2021-01-08 18:39:09 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# install git
+sudo apt-get update && sudo apt-get install -y git subversion
+git config --global user.email "user"
+git config --global user.name "user"
+
+# atom
+wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
+sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
+sudo apt-get update && sudo apt-get install -y atom
