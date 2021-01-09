2acbfdda71588a3ce5acd2805d217dcd2e281c3a
swoitge <66548513+swoitge@users.noreply.github.com>
2021-01-09 23:13:19 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,3 +9,6 @@ git config --global user.name "user"
 wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 sudo apt-get update && sudo apt-get install -y atom
+
+# keepass xc
+sudo apt-get install -y keepassxc
