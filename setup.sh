61c921751dc0cfc094f8b56bd0da8f74bb9768c1
swoitge <66548513+swoitge@users.noreply.github.com>
2021-01-19 21:23:12 +0100

    add required atom plugins

--- setup.sh
+++ setup.sh
@@ -9,6 +9,8 @@ git config --global user.name "user"
 wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 sudo apt-get update && sudo apt-get install -y atom
+apm install svn
+apm install rabbitvcs-svn
 
 # keepass xc
 sudo apt-get install -y keepassxc
