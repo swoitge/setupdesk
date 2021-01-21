5addf4f0d91878bf9f63935b50d7a04fb9ca78b0
swoitge <66548513+swoitge@users.noreply.github.com>
2021-01-21 11:12:32 +0100

    added rabbit vcs

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ git config --global user.email "user"
 git config --global user.name "user"
 
 # atom
+sudo apt-get install -y rabbitvcs-cli rabbitvcs-nautilus
 wget -qO - https://packagecloud.io/AtomEditor/atom/gpgkey | sudo apt-key add -
 sudo sh -c 'echo "deb [arch=amd64] https://packagecloud.io/AtomEditor/atom/any/ any main" > /etc/apt/sources.list.d/atom.list'
 sudo apt-get update && sudo apt-get install -y atom
