def35a8b087210bfc8672d76bc0bf1ffb28bfd14
swoitge <66548513+swoitge@users.noreply.github.com>
2021-04-12 15:38:34 +0200

    added gcloud

--- setup.sh
+++ setup.sh
@@ -12,6 +12,11 @@ echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/
 sudo apt-get update
 sudo apt-get install -y kubectl
 
+# gcloud sdk
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] http://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+sudo apt-get update && sudo apt-get install google-cloud-sdk
+
 #mongodb comapss
 wget https://downloads.mongodb.com/compass/mongodb-compass_1.26.0_amd64.deb
 sudo dpkg -i mongodb-compass_1.26.0_amd64.deb
